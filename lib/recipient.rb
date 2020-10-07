4dadccc463a12ce74c1543e774842856167a51d4
Lina Do <l.do5147@gmail.com>
2020-10-07 11:29:34 -0700

    added details and list all methods for user class

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -10,4 +10,17 @@ class Recipient
     @slack_id = slack_id
     @name = name
   end
+
+  def self.get(url, params)
+    return HTTParty.get(url, query: params)
+  end
+
+  def details
+    raise NotImplementedError, 'Implement me in a child class!'
+  end
+
+  def self.list_all
+    raise NotImplementedError, 'Implement me in a child class!'
+  end
+
 end
--- lib/user.rb
+++ lib/user.rb
@@ -10,5 +10,19 @@ class User < Recipient
     @status_emoji = status_emoji
   end
 
+  def details
+    "Slack ID: #{@slack_id}\nName: #{@name}\nReal Name: #{@real_name}\nStatus Text: #{@status_text}\nStatus Emoji: #{@status_emoji}"
+  end
+
+  def self.list_all
+    url = "https://slack.com/api/users.list"
+    query_params = {
+        token: ENV["SLACK_TOKEN"]
+    }
+
+    response = self.get(url, query_params)
+
+    return response["members"]
+  end
 
 end
