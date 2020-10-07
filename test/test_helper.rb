a2b11883c158905aa9f3c5bc863baaa0a9d1fce5
Taylor Tompkins <taylormililani@gmail.com>
2020-10-07 11:43:09 -0700

    user test file created

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,8 @@ require 'vcr'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require_relative '../lib/user_test'
+
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
   config.hook_into :webmock # tie into this other tool called webmock
---
+++ test/user_test.rb
@@ -0,0 +1,16 @@
+require_relative 'test_helper'
+
+describe "User Class" do
+
+  describe "User Instantiation"  do
+
+    before do
+      @user = ""
+    end
+
+
+  end
+
+
+
+end
